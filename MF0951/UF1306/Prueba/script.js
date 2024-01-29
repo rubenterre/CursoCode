0ad369ca7cef605f836895de7d8d239aa7cc37c3
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2024-01-29 17:56:17 +0100

    Actualización de exercicios

---
+++ MF0951/UF1306/Prueba/script.js
@@ -0,0 +1,7 @@
+document.getElementById('showPopupBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
